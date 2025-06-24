3c005857501e5b03a193466bea11ca0078ba5d5a
CarlosHerazo <Carlosherazo2004@gmail.com>
2025-06-23 23:10:53 -0500

    corregir el nombre del componente Navbar a NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Hero from './Hero';
 import Productos from './Productos';
 import Estadisticas from './Estadisticas';
